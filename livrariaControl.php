ad4b6b8bd3b4274ee0b9d6f8e2411c5390f29ef2
oggy <oggy@oggy>
2018-06-07 00:56:18 +0100

    Primeira parte da pagina inicial

---
+++ livrariaControl.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of livrariaControl
+ *
+ * @author Pedro Moco
+ */
+include_once 'dbconexao.php';
+include_once 'usuario.php';
+include_once 'video.php';
+include_once 'livro.php';
+
+class livrariaControl {
+    //put your code here
+    public function login($usuario,$senha){
+        
+    }
+    public function listarLivros(){
+        
+    }
+    public function pesquisarLivros($c){
+        
+    }
+    public function alterRarLivro(){
+        
+    }
+    
+    public function eliminarLivro(){
+        
+    }
+    
+    public function listarVideo(){
+        
+    }
+    public function pesquisarVIdeo(){
+        
+    }
+    public function alterarVideo(){
+        
+    }
+    public function eliminarVideo(){
+        
+    }
+    public function carregarCarrosel(){
+        
+    }
+    public function alterarCarrosell(){
+        
+    }
+    public function carregarConfigurao(){
+        
+    }
+}
