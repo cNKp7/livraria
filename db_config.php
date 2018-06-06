b2ebcf05d1c6029b117294cb83f7de4cb7aed956
oggy <oggy@oggy>
2018-06-07 00:52:04 +0100

    Primeira parte da pagina inicial

---
+++ db_config.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Pedro Moco
+ */
+interface db_config {
+    //put your code here
+    const PDO='mysql:host=localhost;dbname=mysql';
+    const DB_USUARIO="root";
+    const DB_SENHA="root";
+}
